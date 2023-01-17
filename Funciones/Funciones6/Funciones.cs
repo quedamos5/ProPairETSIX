6e2546a6f722f2233b21d93da9167ca4aef5914b
CH0P0 <gafonsoudev@gmail.com>
2023-01-17 17:16:38 +0000

    ejer 6 en proceso

---
+++ Funciones/Funciones6/Funciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones6
+{
+    class Funciones
+    {
+        public static int ValidateNumb()
+        {
+            int numb = 0;
+            while (!Int32.TryParse(Console.ReadLine(), out numb))
+                Console.WriteLine("Introduce un número entero válido");
+            return numb;
+        }
+
+        public static int FillVector(int[] arr, int numb = 0)
+        {
+            arr[numb] = ValidateNumb();
+            return numb == arr.Length - 1 ? 0 : FillVector(arr, numb + 1);
+        }
+
+        public static int Avg(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum / arr.Length;
+        }
+    }
+}
---
+++ Funciones/Funciones6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Funciones6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el tamaño del vector");
+            int size = Funciones.ValidateNumb();
+            Console.WriteLine("LLena el vector con números enteros");
+            int[] arr = new int[size];
+            Funciones.FillVector(arr, size);
+            Console.WriteLine($"La media de los valores del vector es {Funciones.Avg(arr)}");
+        }
+    }
+}
