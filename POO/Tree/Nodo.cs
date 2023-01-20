e5c131e296f5c75d0b68c9359ab711807551e1c7
CH0P0 <gafonsoudev@gmail.com>
2023-01-20 20:11:12 +0000

    Estructura de arbol en progreso

---
+++ POO/Tree/Nodo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Node
+    {
+        private string data;
+
+        private Node son;
+        private Node sibling;
+
+        public string Data { get => data; set => data = value; }
+        public Node Son { get => son; set => son = value; }
+        public Node Sibling { get => sibling; set => sibling = value; }
+
+        public Node()
+        {
+            data = "";
+            son = null;
+            sibling = null;
+        }
+    }
+}
---
+++ POO/Tree/Tree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Tree
+    {
+        private Node root;
+        private Node work;
+        private int i = 0;
+
+        public Tree() => root = new Node();
+
+        public Node Insert(string data, Node node)
+        {
+            // Si no hay nodo donde insertar, lo tomamos como si fuera en la raiz.
+            if(node == null)
+            {
+                root = new Node();
+                root.Data = data;
+
+                // No hay hijo en la raiz
+                root.Son = null;
+
+                // No hay hermano en la raiz
+                root.Sibling = null;
+
+                return root;
+            }
+
+            // Verificamos que no tiene hijo
+            // Insertamos el dato como hijo
+            if (node.Son == null)
+            {
+                Node temp = new Node();
+                temp.Data = data;
+
+                // Conectamos el nuevo nodo como hijo
+                node.Son = temp;
+
+                return temp;
+            } else // Ya tiene un hijo, lo insertamos como hermano
+            {
+                work = node.Son;
+
+                // Avanzamos hasta el último hermano
+                while (work.Sibling != null)
+                {
+                    work = work.Sibling;
+                }
+
+                //Creamos nodo temporal
+                Node temp = new Node();
+                temp.Data = data;
+
+                // Unimos temp al ultimo hermano
+                work.Sibling = temp;
+                return temp;
+            }
+        }
+    }
+}
